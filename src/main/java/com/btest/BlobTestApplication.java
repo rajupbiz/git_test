ce648c36154e3dc80100ced02edd868b6a87e4c6
rajupbiz <rajupatel.biz@gmail.com>
2016-02-19 14:20:02 +0530

    iii

---
+++ src/main/java/com/btest/BlobTestApplication.java
@@ -0,0 +1,13 @@
+package com.btest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlobTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlobTestApplication.class, args);
+		
+	}
+}
